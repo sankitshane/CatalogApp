707dcfd4ff1d2437bb6502072efb3fc1920e84d0
sankit acharya <sankit.acharya@gmail.com>
2016-12-15 01:51:24 +0530

    templated

--- project.py
+++ project.py
@@ -29,7 +29,7 @@ APPLICATION_NAME = "Restaurant Menu Application"
 @app.route('/')
 @app.route('/catalog')
 def Catalog():
-    return render_template('index.html')
+    return render_template('main.html')
 
 @app.route('/catalog/<int:id>')
 def Item_Catalog(id):
@@ -65,5 +65,6 @@ def delItem(categories_id,id):
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
+    
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
